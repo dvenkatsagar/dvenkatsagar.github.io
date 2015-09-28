2474fdd235469d32e453b37fbdf6a69d38b184fd
Sagar DV <dvenkatsagar@gmail.com>
2015-09-27 22:13:52 -0400

    Updated reveal-chart presentation.

--- prez/reveal-chart/plugin/chartjs/charter.js
+++ prez/reveal-chart/plugin/chartjs/charter.js
@@ -1,86 +1,132 @@
+/*
+    Reveal-chart - A plugin to integrate chartjs with revealjs
+    Copyright (C) 2013-2015  Sagar DV <dvenkatsagar@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 (function(){
-	var options = Reveal.getConfig().chart || {};
-	options.items = options.items || [];
-	var charts = {};
-	var iframes = null;
+	// get default configuration of Reveal.
+	var getDefaults = function(){
 
-	/*
-	Idea is every iframe has 1 chart in it.
-	List of charts. Must be a array of objects as follows :
-		[{
-			selector : null, // The iframe to be used to insert the chart.
-			type : null, // Type of chart, can be of the following values : line, bar, radar, polararea, pie, doughnut.
-			options : {}, // The individual options of each chart. check chartjs documentation.
-			data : []/{}, // The data of the chart. check chartjs documentation.
-		},
-		. . .]
-	*/
-	Reveal.addEventListener("ready", function(event){
-		chartLoad(event);
-		window.charts = charts;
-	});
-	Reveal.addEventListener("slidechanged", function(event){
-		chartLoad(event);
-		window.charts = charts;
-	});
+		// check if chart option is given or not
+	 	var o = Reveal.getConfig().chart || {};
+		o.items = o.items || [];
+		for (var i = 0; i < o.items.length; i++){
+			o.items[i].canvas = o.items[i].canvas || {};
+			o.items[i].canvas.width = o.items[i].canvas.width || "250px";
+			o.items[i].canvas.height = o.items[i].canvas.height || "150px";
+		}
+		return o;
+	}
 
-	function chartLoad(event){
-		// Select all iframes
-		iframes = event.currentSlide.querySelectorAll("iframe");
-		for (var i = 0; i < iframes.length; i++ ){
-			// check if the iframe has data-chart attribute
-			if (iframes[i].hasAttribute("data-chart")){
-				// Load a blank page in it
-				iframes[i].src = "about:blank";
-				// After the page loads
-				iframes[i].addEventListener("load",function(i){
-					// Get iframe document
-					var doc = this.contentDocument || this.contentWindow.document;
-					// write the canvas tag
-					doc.open(); doc.write('<canvas></canvas>'); doc.close();
-					// select the canvas tag
-					var canvas = doc.querySelector("canvas");
-					// set the width and height of the canvas
-					canvas.style.width = (parseInt(this.style.width.slice(0,-2))*90/100).toString() +"px" ||"250px";
-					canvas.style.height = (parseInt(this.style.height.slice(0,-2))*90/100).toString() + "px"||"250px";
-					// get 2d context of canvas
-					ctx = canvas.getContext("2d");
-					// get id of iframe
-					var id = this.getAttribute("id").replace("-","_");
-					// create chart object
-					charts[id] = new Chart(ctx);
-					// if there are any options given in Reveal.initialize
-					if (options.items){
-						// parse options and get informations of the charts to insert
-						for (var j = 0; j < options.items.length; j++){
-							// check the selector
-							if(options.items[j].selector.substring(1) == this.getAttribute("id")){
-								// plot the appropriate graph
-								switch(options.items[j].type){
-									case "line":
-										charts[id].Line(options.items[j].data,options.items[j].options);
-										break;
-									case "bar":
-										charts[id].Bar(options.items[j].data,options.items[j].options);
-										break;
-									case "radar":
-										charts[id].Radar(options.items[j].data,options.items[j].options);
-										break;
-									case "polararea":
-										charts[id].PolarArea(options.items[j].data,options.items[j].options);
-										break;
-									case "pie":
-										charts[id].Pie(options.items[j].data,options.items[j].options);
-										break;
-									case "doughnut":
-										charts[id].Doughnut(options.items[j].data,options.items[j].options);
-										break;
-								}
-							}
-						}
-					}
-				});
+	// Get the chart item related to the iframe
+	var getOptItem = function(opt,id){
+		if (!opt.items){
+			return null;
+		}
+		for (var j = 0; j < opt.items.length; j++){
+			if(opt.items[j].selector.substring(1) == id){
+				return opt.items[j];
 			}
 		}
 	}
+
+	var plotChart = function(item,id){
+		// plot the appropriate graph
+		switch(item.type){
+			case "line":
+				window.charts[id].item.Line(item.data,item.options);
+				break;
+			case "bar":
+				window.charts[id].item.Bar(item.data,item.options);
+				break;
+			case "radar":
+				window.charts[id].item.Radar(item.data,item.options);
+				break;
+			case "polararea":
+				window.charts[id].item.PolarArea(item.data,item.options);
+				break;
+			case "pie":
+				window.charts[id].item.Pie(item.data,item.options);
+				break;
+			case "doughnut":
+				window.charts[id].item.Doughnut(item.data,item.options);
+				break;
+		}
+	}
+
+	// Load chart into iframe
+	var loadChart = function(event){
+		// Get all iframes
+		var iframes = event.currentSlide.querySelectorAll("iframe");
+		for (var i = 0; i < iframes.length; i++ ){
+
+			// check if iframe has data-chart attribute
+			if (iframes[i].hasAttribute("data-chart")){
+
+				// load a blank page into iframe
+				iframes[i].src = "about:blank";
+				iframes[i].addEventListener("load",function load(e){
+
+					// remove event listener after loading to that it doesnt fire multiple times
+					e.target.removeEventListener("load",load,false);
+
+					// get id of frame
+					var id = e.target.getAttribute("id").replace("-","_").toString();
+
+					// create charts object with id as property
+					window.charts[id] = {};
+
+					// get iframe document
+					window.charts[id].doc = e.target.contentDocument || e.target.contentWindow.document;
+
+					// write empty canvas as iframe contents
+					window.charts[id].doc.open();
+					window.charts[id].doc.write('<canvas></canvas>');
+					window.charts[id].doc.close();
+
+					// get iframe canvas
+					window.charts[id].canvas = window.charts[id].doc.querySelector("canvas");
+
+					// get canvas context
+					window.charts[id].ctx = window.charts[id].canvas.getContext("2d");
+
+					// create new chart with context
+					window.charts[id].item = new Chart(window.charts[id].ctx);
+
+					// get the appropriate item data
+					var optitem = getOptItem(getDefaults(),e.target.getAttribute("id"));
+
+					if (optitem != null){
+						// change canvas size
+						window.charts[id].canvas.style.width = optitem.canvas.width;
+						window.charts[id].canvas.style.height = optitem.canvas.height;
+
+						// plot chart
+						plotChart(optitem,id);
+					}
+					console.log(window.charts);
+				},false);
+			}
+		}
+	}
+
+	// main
+	window.charts = {};
+	Reveal.addEventListener("ready", function(event){
+		loadChart(event);
+		Reveal.addEventListener("slidechanged", loadChart,false);
+	},false);
 })();
