bcf9546a4448a0cf0af4f93dbcd12fca11bf6224
Sagar DV <dvenkatsagar@gmail.com>
2015-10-16 19:12:05 -0400

    Understood that mobile has settings to get fullscreen experience

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
-function hideAddressBar()
-{
-  if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-      }
-
-      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-  }
-}
-
 $(document).ready(function(){
-  window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-  window.addEventListener("orientationchange", hideAddressBar );
+
   $('#home .carousel').slick({
     infinite: false,
     speed: 300,
